c94eac961555ff6e2c06601fb1f7aaf501017c42
game <supavit13@gmail.com>
2018-08-03 15:38:13 +0700

    game

--- adsbcheck.py
+++ adsbcheck.py
@@ -8,7 +8,7 @@ from datetime import datetime
 import logging
 o = os.popen('curl 127.0.0.1:8080/data.json').read()
 fail = "Failed"
-print(type o.find(fail))
+type(o)
 if o.find(fail) == -1:
     print("running")
 else:
