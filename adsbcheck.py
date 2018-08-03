9748a06b1a130509223e4f7cc7b04c6fa832f7aa
game <supavit13@gmail.com>
2018-08-03 15:47:04 +0700

    game

--- adsbcheck.py
+++ adsbcheck.py
@@ -6,12 +6,17 @@ from subprocess import call
 from time import sleep, time
 from datetime import datetime
 import logging
-o = os.popen('curl 127.0.0.1:8080/data.json').read()
-fail = "Failed"
-type(o)
-if o.find(fail) == -1:
-    print("running")
-else:
-    print(fail)
+try:
+
+    o = requests.get('127.0.0.1:8080/data.json')
+    print(o.json())
+except:
+    print("failed")
     os.popen('sudo systemctl restart dump1090.service')
+
+# if o.find("fail") == -1:
+#     print("running")
+# else:
+#     print(fail)
+#     os.popen('sudo systemctl restart dump1090.service')
     
