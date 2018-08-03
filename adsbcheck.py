7a888791f6b2c7dcbc0f71f41e90f6b50b9ed227
game <supavit13@gmail.com>
2018-08-03 15:34:42 +0700

    game

--- adsbcheck.py
+++ adsbcheck.py
@@ -10,7 +10,8 @@ o = os.popen('curl 127.0.0.1:8080/data.json').read()
 fail = "Failed"
 print(o.find(fail))
 if o.find(fail) == -1:
+    print("running")
+else:
     print(fail)
     os.popen('sudo systemctl restart dump1090.service')
-else:
-    print("running")
+    
