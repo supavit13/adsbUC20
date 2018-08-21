1c2cf84879f166a4e51cfdb843f363f1c0ffd89b
game <supavit13@gmail.com>
2018-08-21 10:41:12 +0700

    game

--- adsbcheck.py
+++ adsbcheck.py
@@ -6,13 +6,39 @@ from subprocess import call
 from time import sleep, time
 from datetime import datetime
 import logging
+if os.path.isfile('/home/pi/checking.log') and os.path.getsize("/home/pi/checking.log") >= 10000000:
+    open("/home/pi/checking.log", "w").close()
+    logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(message)s',
+                    filename='/home/pi/checking.log',
+                    filemode='w')
+if os.path.isfile('/home/pi/checking.log'):
+    logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(message)s',
+                    filename='/home/pi/checking.log',
+                    filemode='a')
+    logging.info('ADS-B Checker starting...')
+else:
+    logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)s %(message)s',
+                    filename='/home/pi/checking.log',
+                     filemode='w')
+    logging.debug('ADS-B Checker starting...')
+    logging.info('Create file checking.log')
 try:
-
     o = requests.get('http://127.0.0.1:8080/data.json')
+    logging.debug('ADS-B Checker : Not error...')
     print(o.json())
 except:
-    print("failed")
+    logging.debug('ADS-B Checker : dump1090.service error restart now')
     os.popen('sudo systemctl restart dump1090.service')
+    os.popen('sudo systemctl start dump1090.service')
+try:
+    urllib.request.urlopen("https://www.google.com/")
+except urllib.error.URLError:
+    logging.warning("Network down")
+else:
+    logging.info("Up and running")
 
 # if o.find("fail") == -1:
 #     print("running")
