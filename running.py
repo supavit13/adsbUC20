b629521585a9a0c828a1430570bf59d22a37813d
game <supavit13@gmail.com>
2018-08-21 12:10:30 +0700

    game

--- running.py
+++ running.py
@@ -9,12 +9,12 @@ import logging
 o =os.popen("ps ax | grep By").read()
 print(o.find("adsbUC20"))
 err = o.find("adsbUC20")
-if err != -1:
-	print("error")
+if err == -1:
+	print("not found script")
 	# os.popen("sudo pkill -f By3G.py")
 	# sleep(0.1)
 	# os.popen("sudo python3 /home/pi/adsbUC20/By3G/By3G.py 164.115.43.87 8080 1 Vimt29H2p9 5b45b88c96f2234c3b3a8151 &")
 else:
-	print("not error")
+	print("script running")
 #sudo python3 /home/pi/adsbUC20/By3G/By3G.py 164.115.43.87 80
 #80 1 Vimt29H2p9 5b45b88c96f2234c3b3a8151
