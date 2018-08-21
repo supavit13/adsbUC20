951371416a1ffc5bb3f79d0ea37c3470ae409586
game <supavit13@gmail.com>
2018-08-21 12:06:40 +0700

    game

--- running.py
+++ running.py
@@ -7,13 +7,13 @@ from datetime import datetime
 import logging 
 
 o =os.popen("ps ax | grep By").read()
-print(o.find("?"))
-err = o.find("?")
+print(o.find("adsbUC20"))
+err = o.find("adsbUC20")
 if err != -1:
 	print("error")
-	os.popen("sudo pkill -f By3G.py")
-	sleep(0.1)
-	os.popen("sudo python3 /home/pi/adsbUC20/By3G/By3G.py 164.115.43.87 8080 1 Vimt29H2p9 5b45b88c96f2234c3b3a8151 &")
+	# os.popen("sudo pkill -f By3G.py")
+	# sleep(0.1)
+	# os.popen("sudo python3 /home/pi/adsbUC20/By3G/By3G.py 164.115.43.87 8080 1 Vimt29H2p9 5b45b88c96f2234c3b3a8151 &")
 else:
 	print("not error")
 #sudo python3 /home/pi/adsbUC20/By3G/By3G.py 164.115.43.87 80
